56bcf22b9a48981c8abd0ca92003446897418cfa
valentinKrajzelman <valentinkrajzelman@hotmail.com>
2024-02-24 12:10:54 -0300

    cosas

--- src/components/help.tsx
+++ src/components/help.tsx
@@ -2,7 +2,14 @@
 
 const Help = () => {
   return (
-    <div>Help</div>
+    <div>
+      Bienvenido!
+      <div>
+      Preparate para la ultima experiencia digital, en "Pregunta en la red" vas a poder surfear la red y hacer cualquier consulta que tengas.
+      Y la red va a elegir de un sinfin de posibilidades la que necesitas.
+      </div>
+
+    </div>
   )
 }
 
