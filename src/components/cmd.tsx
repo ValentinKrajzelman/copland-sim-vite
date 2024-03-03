c48670e07eecbad9dee4b0a525dcc1002e34c1ab
valentinKrajzelman <valentinkrajzelman@hotmail.com>
2024-03-03 17:22:19 -0300

    cosas

--- src/components/cmd.tsx
+++ src/components/cmd.tsx
@@ -1,6 +1,7 @@
 import { useState, ChangeEvent, useEffect } from "react";
 
 const Cmd = () => {
+
   const [text, setText] = useState(
     "Copland OS.                                                  v.2.556.8588 \n(C) Copland Corp. All rigths reserved.\n \n \n \n \n".split(
       /\n/,
@@ -9,41 +10,43 @@ const Cmd = () => {
   const [input, setInput] = useState("C:\\User> ");
 
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    if (e.target.value[e.target.value.length - 1] == "\n" ) {
-      console.log()
-      switch(e.target.value){
-        case "C:\\User> console log something\n": 
-        setText([...text, input, 'something']);
-        setInput("C:\\User> ");
+    if (e.target.value[e.target.value.length - 1] == "\n") {
+      switch ((e.target.value).replace(/C:\\User> (.*)/s,'$1').trim()) {
+        case "-color blue":
+          setText([...text, input, "something"]);
+          setInput("C:\\User> ");
+          break;
+        case "-help":
+          setText([...text, input, "-color [blue,red,green] //change console color\n-run [links, help, mixer, ask] //run de desired program\n-off //powers off the OS"]);
+          setInput("C:\\User> ");
           break;
         default:
-          setText([...text, input, 'no command found! \n to see commands use -help']);
+          setText([
+            ...text,
+            input,
+            "no command found! \n to see commands use -help",
+          ]);
           setInput("C:\\User> ");
-
       }
-
-
-
-    } 
-    else if (e.target.value.length > 73) {
+    } else if (e.target.value.length > 73) {
       setText([...text, input]);
       setInput("");
-    }
-  else {
+    } else if (e.target.value.length < 10) {
+      setInput("C:\\User> ");
+    } else {
       setInput(e.target.value);
     }
   };
 
   return (
-    <div className="h-[35rem] w-[38rem] bg-[#D9D9D9] bg-opacity-90">
-      <div className="relative h-full w-full">
+    <div className="h-[35rem] w-[38rem] bg-[#D9D9D9] bg-opacity-90 overflow-y-scroll">
+      <div className="relative min-h-full h-fit w-full">
         <textarea
           value={input}
           onChange={handleChange}
-          className="absolute left-0 top-0 z-10 h-full w-full resize-none overflow-hidden border-none bg-transparent text-transparent outline-none "
+          className="absolute left-0 bottom-0 z-10 h-full w-full resize-none overflow-hidden border-none bg-transparent text-transparent outline-none "
         />
         {text.map((line, index) => {
-          
           return (
             <div
               key={index}
